9bb54fc24fa323ad25b1dc646f4a03e624b726a9
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-04-02 21:19:06 +0300

    Debugging

--- src/app/date.pipe.ts
+++ src/app/date.pipe.ts
@@ -7,10 +7,10 @@ export class DatePipe implements PipeTransform {
 
   transform(value: any): number {
     let now:any = new Date();
-      var diff = now.getTime() - value.getTime();
-      diff/=1000;
-      diff/=(60*60)
-      diff/=24;
-      return Math.ceil(diff) - 1;
+      var difference = now.getTime() - value.getTime();
+      difference= difference/1000;
+      difference= difference/(60*60)
+      difference= difference/24;
+      return Math.ceil(difference) - 1;
   }
 }
